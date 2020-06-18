7c753d8b073ed02e28629daf6a6c14c095c34185
mounica56 <66748069+mounica56@users.noreply.github.com>
2020-06-18 19:14:55 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCrN3ybt8SZYeN5nHqIYspQPU3Mv7jSF-U",
-    authDomain: "oshop-d3065.firebaseapp.com",
-    databaseURL: "https://oshop-d3065.firebaseio.com",
-    projectId: "oshop-d3065",
-    storageBucket: "oshop-d3065.appspot.com",
-    messagingSenderId: "461632192728",
-    appId: "1:461632192728:web:8d56754606a3d859fb17ad",
-    measurementId: "G-58K5MV1YZ1"
+   // Put your Firebase config properties here
   }
 };
